bcf207b7e1970fc09df68a5b8c70dd439fa9b240
tiagoze <150803206+tiagoze@users.noreply.github.com>
2023-11-20 17:42:52 -0300

    Update quemsomos.php

--- quemsomos.php
+++ quemsomos.php
@@ -1,17 +1,22 @@
 
-<div style="background-color:green;">
-
-<p style="color:green;"><h1>QUEM SOMOS?</h1> </p>
-</div>
-
-<div style="background-color:limegreen;">
-<p style="color:black;"><h3>O que trazemos para as pessoas que nos acompanha?<h3> </p>
+<style>
+    body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg, cyan, green);
+        }
+</style>
+<center>
+    <br><br><br><br>
+    <p> <h1>QUEM SOMOS</h1> </p> <br>
+    <p style="color:black;"><h3>O que trazemos para as pessoas que nos acompanha?<h3> </p>
 <P><h5> Nós trazemos todas as notícias mais atualizadas sobre o futebol mundial, ou seja, trazemos notícias sobre contratações, títulos, e jogos importantes que irão acontecer.<h5></p>
 
 <p><h3>Como surgiu o nosso site de notícias <h3> </p>
-<p><h5>Criamos nosso site através de incentivos de amigos e parentes, que virão que tinhamos potencial para entrar nesse ramo de noticiar fatos do futebol.<h5></p>
+<p><h5>Criamos nosso site através de incentivos de amigos e parentes, que viram que tinhamos potencial para entrar nesse ramo de noticiar fatos do futebol.<h5></p>
 
 <p><h3> Com mais de 5 anos de grupo, o FUTNEWS vem sendo o maior grupo de notícias dos últimos tempos,sendo o maior grupo de notícias no ano de 2022 e 2023. O grupo é formado por dois integrantes:<h3></p>
 
 <p>Vitor Nóbrega De Souza </p>
+<br>
 <p>José Tiago Ferreira Santos </p> 
+</center>
