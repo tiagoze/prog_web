e55d4b0ec436bfea3559ec55795b4daeb277f8e2
tiagoze <150803206+tiagoze@users.noreply.github.com>
2023-11-20 17:42:18 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,9 +1,28 @@
-<div style="background-color:MediumSeaGreen;">
+<style>
+    body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg, cyan, green);
+        }
+        div{
+            background-color: rgba(0,0,0,0.5);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            padding: 80px;
+            color: white;
+        }
+        input{
+            border: none;
+            outline: none;
+        }
+</style>
 
+
+<div>
 <h1>FAÇA SEU CADASTRO </h1>
 <h3>Preencha as lacunas com seus dados.<h3>
 <form action="envia.php" method="post">
-
 <label>nome:</label>
 <input type="text" name="nome"><br>
 <label>idade:</label>
@@ -12,8 +31,5 @@
 <input type="text" name="email"><br>
 <br>
 <input type="submit" value="ENVIAR">
-
-
-
-
 </form>
+</div>
