964ff50cce30fd1486e22f9efb4e217d4d5b2215
tiagoze <150803206+tiagoze@users.noreply.github.com>
2023-11-20 13:24:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
-<?php
-    include_once("topo.php");
-    include_once("menu.php");
+<!DOCTYPE html>
+<html>
+    <head lang = "pt-br">
+        <meta charset="utf-8">
+        <meta nome="viewport" content="width=device-width, initial-scale=1.0">
+        <link href ="css/style.css" rel="stylesheet">
+        <title>FUT NEWS</title>
+     </head>
+<body>
+    <header>
+        <nav class="menu">
+            <a href="" class="logo">LOGO</a>
 
-    if(empty($_SERVER["QUERY_STRING"])){
-        $var = "conteudo.php";
-        include_once("$var");
-
-}else{
-    $pg = $_GET['pg'];
-    include_once("$pg.php");
-}
-    include_once("rodape.php");
-
-?>
+            <ul>
+                <li><a href="home" class="ativo">HOME</a></li>
+                <li><a href="noticias.php">NOTÍCIAS</a></li>
+                <li><a href="quemsomos.php">QUEM SOMOS</a></li>
+                <li><a href="sejapro.php">SEJA PRO</a></li>
+            </ul>
+        </nav><!--menu-->
+        <section class = "principal">
+            <h2>SEJA PRO !!!</h2>
+            <div class="links">
+                <a href="sejapro.php" > Se cadastre!</a>
+            </div>
+        </section><!--principal-->
+    </header>
+</body>
+</html>
