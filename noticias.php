16bfe76809b5d44866be8c3d5cbfb5c7e36ba363
tiagoze <150803206+tiagoze@users.noreply.github.com>
2023-11-20 13:25:59 -0300

    Update and rename menu.php to noticias.php

---
+++ noticias.php
@@ -0,0 +1,39 @@
+<div style="background-color: MediumSeaGreen	; background-image: url(''); background-size: cover; background-position: center; background-repeat: no-repeat;"> 
+
+<p style="color:black;"><h2>Ultimas Noticias da Semana:<h2>-repeat: no-repeat; 
+</p>
+
+<p>
+  <h3>Parar Messi? Uruguaio é sincero: "Não sei como, nunca pude"<h3>
+</p>
+<img width="450px" src="https://s2-ge.glbimg.com/EEQOG-DljeEGSjq8QCuQQXHDous=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/v/9/MUpKHAQ3OAp6kRMzOS7w/gettyimages-1182711380.jpg">
+
+<p>
+  <h3>Moto Club deve receber "casa nova" antes de começar a pré-temporada 2024</h3>
+</p>
+<iframe width="560" height="315" src="https://s2-ge.glbimg.com/YqKjX52EN3DTo7mvi7d0d2NKc3I=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/D/F/i68g1dTXikT3wipU0y0w/sede-do-moto.jpeg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+<p>
+  <h3>Comercial diz que reposição de cargo que era de Marciano é incerta</h3>
+  </p>    
+<iframe width="1090" height="607" src="https://s2-ge.glbimg.com/LKcoz0fkssUIcJPiHwTCrid8Dj4=/1080x608/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/d/X/MpehptSoCixyRC4KQNzA/mg-0158.jpg" allowfullscreen></iframe>
+
+<p>
+  <h3>Serra Branca anuncia atacante Marcelo Toscano e meia Anderson Paraíba para o elenco 2024</h3>
+
+  </p>
+<iframe width="560" height="315" src="https://s2-ge.glbimg.com/6-OALf9IObSw9rwm6NGatf1ZfvA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/5/D/Y0TUbtThuq2DyJxgsFWw/354768284-1011304286534550-3898866902273031137-n.jpg"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+<p>
+  <h3>Baier, Adilson e Chamusca: relembre desempenho dos técnicos do Botafogo-SP em 2023</h3>
+     </p>
+<iframe width="1050" height="600" src="https://s2-ge.glbimg.com/01bynnzg8QZagnNgt0FH4sissVE=/0x0:1700x1065/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/b/0/9JBhj8S4ebftOvSBfKUw/design-sem-nome-60-.png" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+<p><h3>Vinicius Junior promete camisa a fã que teve presente "roubado" em jogo do Real Madrid</h3> </p>
+<iframe width="1000" height="650" src="https://uploads.metropoles.com/wp-content/uploads/2022/07/03134947/GettyImages-1400978549.jpg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+<p><h3>Brasileirão pausa para Data Fifa: saiba quando seu time volta a campo…</h3></p>
+<iframe width="1000" height="580" src="https://conteudo.imguol.com.br/c/esporte/fd/2023/11/11/endrick-e-ze-rafael-comemoram-gol-do-palmeiras-contra-o-inter-pelo-brasileirao-1699753210746_v2_900x506.jpg.webp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    
+
+
+</div>
