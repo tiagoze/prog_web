8b9350976b48d451be4bfad34715dca4af437d27
tiagoze <150803206+tiagoze@users.noreply.github.com>
2023-11-20 17:09:37 -0300

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,19 @@
+<div style="background-color:MediumSeaGreen;">
+
+<h1>FAÇA SEU CADASTRO </h1>
+<h3>Preencha as lacunas com seus dados.<h3>
+<form action="envia.php" method="post">
+
+<label>nome:</label>
+<input type="text" name="nome"><br>
+<label>idade:</label>
+<input type="text" name="idade"><br>
+<label>email:</label>
+<input type="text" name="email"><br>
+<br>
+<input type="submit" value="ENVIAR">
+
+
+
+
+</form>
